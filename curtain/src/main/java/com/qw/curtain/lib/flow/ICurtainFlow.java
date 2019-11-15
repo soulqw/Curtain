aa16c2d216548fa9950cbd976086234ea1d56ceb
George <cd5160866@126.com>
2019-11-15 18:57:18 +0800

    1.[ADD]add annotations

--- curtain/src/main/java/com/qw/curtain/lib/flow/ICurtainFlow.java
+++ curtain/src/main/java/com/qw/curtain/lib/flow/ICurtainFlow.java
@@ -10,14 +10,32 @@ import androidx.annotation.IdRes;
  */
 public interface ICurtainFlow {
 
+    /**
+     * 推送到下个
+     */
     void push();
 
+    /**
+     * 回到上个
+     */
     void pop();
 
+    /**
+     * 按照id 去某个节点
+     *
+     * @param curtainId
+     */
     void toNodeById(int curtainId);
 
+    /**
+     * 找到当前展示curtain 中到view元素
+     */
     <T extends View> T findViewInCurrentCurtain(@IdRes int id);
 
+    /**
+     * 结束
+     */
     void finish();
+
 
 }
